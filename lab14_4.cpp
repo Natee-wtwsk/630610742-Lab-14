fe34ef827382371d040f682217e446a0677f14a6
Natee <gg977120@gmail.com>
2021-02-02 16:03:11 +0700

    wirte shuffle

--- lab14_4.cpp
+++ lab14_4.cpp
@@ -17,3 +17,30 @@ int main(){
 	
 	return 0;
 }
+
+void shuffle(int &a,int &b,int &c,int &d)
+{
+	int gift[4] = {a,b,c,d};
+	for(int i = 0;i < 4;i++)
+	{
+		if(rand() %10 > 5)
+		{
+			int temp_money = gift[i];
+			if(i == 3)
+			{
+				gift[i] = gift[0];
+				gift[0] = temp_money;
+			}
+			else
+			{
+				gift[i] = gift[i+1];
+				gift[i+1] = temp_money;
+			}
+		}
+	}
+
+	a = gift[0];
+	b = gift[1];
+	c = gift[2];
+	d = gift[3];
+}
